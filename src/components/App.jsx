ffd75583dd80b7c90fcbbff5aaedf1e3892d269f
Slava <110696118+sl7one@users.noreply.github.com>
2022-12-21 15:02:16 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,40 @@
-import React from 'react';
+import { useState } from 'react';
 
-import SearchForm from 'components/Form/Form';
-import HeroesList from 'components/HeroesList/HeroesList';
-import { ModalItem } from './Modal/Modal';
+const App = () => {
+  const [isExpand, setIsExpand] = useState(false);
 
-export class App extends React.Component {
-  state = {
-    query: '',
-    modalShow: false,
-    hero: {},
-  };
+  // useEffect()
 
-  onSubmit = query => {
-    this.setState({ query });
-  };
-
-  setModalShow = hero => {
-    this.setState({ modalShow: true, hero });
-  };
-
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          alignItems: 'center',
-          fontSize: 20,
-          color: '#010101',
-        }}
-      >
-        <SearchForm onSubmit={this.onSubmit} />
-        <HeroesList query={this.state.query} onClick={this.setModalShow} />
-        <ModalItem
-          hero={this.state.hero}
-          show={this.state.modalShow}
-          onHide={() => this.setState({ modalShow: false })}
-        />
+  return (
+    <div className="container">
+      <div className="accordion">
+        <div className="accordion-item">
+          <p className="title" onClick={() => setIsExpand(!isExpand)}>
+            First Question
+          </p>
+          {isExpand ? (
+            <div className="content">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                eu interdum diam. Donec interdum porttitor risus non bibendum.
+                Maecenas sollicitudin eros in quam imperdiet placerat. Cras
+                justo purus, rhoncus nec lobortis ut, iaculis vel ipsum. Donec
+                dignissim arcu nec elit faucibus condimentum. Donec facilisis
+                consectetur enim sit amet varius. Pellentesque justo dui,
+                sodales quis luctus a, iaculis eget mauris. Aliquam dapibus,
+                ante quis fringilla feugiat, mauris risus condimentum massa, at
+                elementum libero quam ac ligula. Pellentesque at rhoncus dolor.
+                Duis porttitor nibh ut lobortis aliquam. Nullam eu dolor
+                venenatis mauris placerat tristique eget id dolor. Quisque
+                blandit adipiscing erat vitae dapibus. Nulla aliquam magna nec
+                elementum tincidunt.
+              </p>
+            </div>
+          ) : null}
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default App;
